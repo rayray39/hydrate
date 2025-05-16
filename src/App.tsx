7ad648c89ca94fcc656bfa3fac8f534a9f13894b
rayray39 <raynerlimfy@gmail.com>
2025-05-16 21:38:36 +0800

    App.tsx: Install mantine ui and test slider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { Slider } from '@mantine/core';
 import './App.css'
 
 function App() {
+    return <MantineProvider>
+        {'Hello World'}
 
-  return (
-    <>
-        Hello World
-    </>
-  )
+        <Slider 
+            color="blue"
+            defaultValue={40}
+            marks={[
+                { value: 20, label: '20%' },
+                { value: 50, label: '50%' },
+                { value: 80, label: '80%' },
+            ]}
+        />
+    </MantineProvider>;
 }
 
 export default App
