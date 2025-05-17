4d54ad69cd3ed88e493604b7b6dd802cee1df0ad
rayray39 <raynerlimfy@gmail.com>
2025-05-17 14:57:27 +0800

    vertical-slider.tsx: Add icon and label to vertical slider

--- src/vertical-slider.tsx
+++ src/vertical-slider.tsx
@@ -18,7 +18,7 @@ function VerticalSlider() {
             justifyContent:'center'
         }}>
             <Group justify="center">
-                {/* background */}
+                {/* Background */}
                 <div
                     ref={ref}
                     style={{
@@ -29,36 +29,57 @@ function VerticalSlider() {
                         borderRadius: SLIDER_BORDER_RADIUS
                     }}
                 >
-                {/* Filled bar */}
-                <div
-                    style={{
-                        position: 'absolute',
-                        bottom: 0,
-                        height: `${value * 100}%`,
-                        width: SLIDER_WIDTH,
-                        backgroundColor: 'var(--mantine-color-blue-filled)',
-                        opacity: 0.7,
-                        borderRadius: SLIDER_BORDER_RADIUS
-                    }}
-                />
+                    {/* Icon */}
+                    <div
+                        style={{
+                            position:'absolute',
+                            width:'100%',
+                            textAlign:'center',
+                            top: 10,
+                        }}
+                        className='vert-slider-icon'
+                    >{"💧"}</div>
+                    {/* Label */}
+                    <div
+                        style={{
+                            position:'absolute',
+                            width:'100%',
+                            top: 30,
+                            textAlign:'center'
+                        }}
+                        className='vert-slider-label'
+                    >{"water"}</div>
 
-                {/* Thumb */}
-                <div
-                    style={{
-                        position: 'absolute',
-                        bottom: `calc(${value * 100}%)`,
-                        left: 0,
-                        width: SLIDER_WIDTH,
-                        height: THUMB_HEIGHT,
-                        backgroundColor: 'var(--mantine-color-gray-7)',
-                        borderRadius: SLIDER_BORDER_RADIUS
-                    }}
-                />
+                    {/* Filled bar */}
+                    <div
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            height: `${value * 100}%`,
+                            width: SLIDER_WIDTH,
+                            backgroundColor: 'var(--mantine-color-blue-filled)',
+                            opacity: 0.7,
+                            borderRadius: SLIDER_BORDER_RADIUS
+                        }}
+                    />
+
+                    {/* Thumb */}
+                    <div
+                        style={{
+                            position: 'absolute',
+                            bottom: `calc(${value * 100}%)`,
+                            left: 0,
+                            width: SLIDER_WIDTH,
+                            height: THUMB_HEIGHT,
+                            backgroundColor: 'var(--mantine-color-gray-7)',
+                            borderRadius: SLIDER_BORDER_RADIUS
+                        }}
+                    />
                 </div>
             </Group>
 
             <Text ta="center" mt="sm">
-                Value: {Math.round(value * 100)}
+                {Math.round(value * 100)} litres
             </Text>
         </Box>
     )
