eaf98f6df384f2c3efa962d84c9aacd06c086df8
rayray39 <raynerlimfy@gmail.com>
2025-05-19 14:40:03 +0800

    server.cjs: Set up express backend server

---
+++ src/server.cjs
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const hydrationRouter = require('./routes/hydration.cjs');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// if the url endpoint starts with '/api/hydration', direct it to the hydrationRouter
+app.use('/api/drinks', hydrationRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
